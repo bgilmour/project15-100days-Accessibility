5bc34699fdc70d972b43ee03e886452cd346b83d
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-02 00:45:41 +0100

    Initial Commit

---
+++ Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Bruce Gilmour on 2021-08-02.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Bruce Gilmour on 2021-08-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
